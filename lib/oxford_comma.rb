bbe16bdead5684a1cff65f9ca75564a01e363a2b
Scott Harrison <s.z.r.harrison@gmail.com>
2017-03-07 19:09:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  string = array[0]
+  if array.length == 2
+    string = array.join( ' and ' )
+  elsif array.length > 2
+    array << "and #{ array.pop }"
+    string = array.join( ', ' )
+  end
+  string
 end
